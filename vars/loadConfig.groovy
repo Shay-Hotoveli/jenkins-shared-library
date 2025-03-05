da97629969b759b166e515410c9b9daf492e8c2d
Shay-Hotoveli <shayhotav@gmail.com>
2025-03-05 20:18:39 +0200

    addin config.json and loadConfig.groovy

---
+++ vars/loadConfig.groovy
@@ -0,0 +1,6 @@
+import groovy.json.JsonSlurper
+
+def call(String fileName = 'config.json') {
+    def fileContent = libraryResource(fileName)
+    return new JsonSlurper().parseText(fileContent)
+}
